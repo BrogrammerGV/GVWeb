a59131772a41faaeff8bf760d28f8b0cad11154a
BrogrammerGV <thebrogrammer@mac.local>
2017-08-27 15:26:59 -0500

    Initial Commit: for Acesg

---
+++ php/twitter/config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Your Twitter App Info
+ * How to create Twitter API key you can read here (or use google):  - http://www.gabfirethemes.com/create-twitter-api-key/
+ */
+
+ // User Name
+ define('GVUeSports', 'Colejmc13');
+
+// Consumer Key
+define('CONSUMER_KEY', 'GP8mKDlFDB8GuIZ5c69GA49sd');
+define('CONSUMER_SECRET', 'Bg0AWMlZ4rFMgViIA2EOi2aU8BcLKsWpAAGsQSP1PFpRq1YvpN');
+
+// User Access Token
+define('ACCESS_TOKEN', '836676815800389633-mZUh9pWn1LqTQEjb0XOB6w6zM18nI1n');
+define('ACCESS_SECRET', 'gEP9wVSfrNDOKBQjsCUZybL9jOAjpGz98aC256snprfqi');
+
+// Cache Settings
+define('CACHE_ENABLED', true);
+define('CACHE_LIFETIME', 3600); // in seconds
+define('HASH_SALT', md5(dirname(__FILE__)));
